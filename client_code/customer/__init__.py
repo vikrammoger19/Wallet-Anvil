f58c42a83298b860290ab9d23b73483a45a0908b
rathourpankaj1999@gmail.com <rathourpankaj1999@gmail.com>
2024-02-02 08:53:50 +0000

    Created Transaction_history; Edited customer

--- client_code/customer/__init__.py
+++ client_code/customer/__init__.py
@@ -36,3 +36,7 @@ class customer(customerTemplate):
     def button_2_click(self, **event_args):
       """This method is called when the button is clicked"""
       pass
+
+    def button_3_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      open_form('Transaction_history')
