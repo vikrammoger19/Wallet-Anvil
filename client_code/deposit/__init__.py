0dc260b1878effe2028143e5c5aac62c54730954
sachinnaik964@gmail.com <sachinnaik964@gmail.com>
2024-02-07 03:41:43 +0000

    Edited deposit and transfer

--- client_code/deposit/__init__.py
+++ client_code/deposit/__init__.py
@@ -5,6 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from datetime import datetime
+import anvil.http
 class deposit(depositTemplate):
 
     def __init__(self,user=None, **properties):
