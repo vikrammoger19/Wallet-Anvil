81f8c51d5699426422145a81f0ddb46c772db837
rathourpankaj1999@gmail.com <rathourpankaj1999@gmail.com>
2024-02-07 10:45:25 +0000

    Edited admin.account_management, admin.admin_add_user, admin.admin_view, admin.audit_trail and 6 more

--- client_code/admin/admin_view/__init__.py
+++ client_code/admin/admin_view/__init__.py
@@ -286,7 +286,7 @@ class admin_view(admin_viewTemplate):
             )
   
     def link_1_click(self, **event_args):
-        open_form('Home')
+        open_form('admin.report_analysis')
 
     def link_8_copy_click(self, **event_args):
         """This method is called when the link is clicked"""
@@ -298,4 +298,31 @@ class admin_view(admin_viewTemplate):
 
     def button_8_click(self, **event_args):
         """This method is called when the button is clicked"""
-        open_form('Home')
+        open_form('Login')
+
+    def link_2_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      open_form('admin.account_management', user= self.user)
+
+    def link_7_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      open_form('admin.transaction_monitoring')
+
+    def link_6_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      open_form('admin.admin_add_user')
+
+    def link_5_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      open_form('admin.audit_trail', user = self.user)
+
+    def link_4_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      serves_data = app_tables.sevices.search()
+
+    # Open the admin.user_support form and pass the serves_data
+      user_support_form = open_form('admin.user_support', serves_data=serves_data)
+
+    def link_3_click(self, **event_args):
+      """This method is called when the link is clicked"""
+      show_users_form = open_form('admin.show_users')
--- client_code/admin/show_users/__init__.py
+++ client_code/admin/show_users/__init__.py
@@ -40,3 +40,42 @@ class show_users(show_usersTemplate):
   def button_1_copy_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.admin_add_user')
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.report_analysis')
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.account_management', user= self.user)
+
+  def link_7_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.transaction_monitoring')
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.admin_add_user')
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.audit_trail', user = self.user)
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    serves_data = app_tables.sevices.search()
+
+    # Open the admin.user_support form and pass the serves_data
+    user_support_form = open_form('admin.user_support', serves_data=serves_data)
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    show_users_form = open_form('admin.show_users')
+
+  def link_8_copy_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin')
+
+  def button_8_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Login')
