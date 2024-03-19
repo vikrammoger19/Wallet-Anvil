00ab024f2c06c591e9408c1ed3cf8bc1767bbedf
kohilain16@gmail.com <kohilain16@gmail.com>
2024-03-19 12:38:48 +0000

    Edited admin.add_currency and admin.add_currency.ItemTemplate8

--- client_code/admin/add_currency/ItemTemplate8/__init__.py
+++ client_code/admin/add_currency/ItemTemplate8/__init__.py
@@ -1,10 +1,30 @@
 from ._anvil_designer import ItemTemplate8Template
 from anvil import *
 import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class ItemTemplate8(ItemTemplate8Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.user_count_displayed = False
 
-    # Any code you write here will run before the form opens.
+  def view_user_click(self, **event_args):
+        if not self.user_count_displayed:
+            selected_currency = self.item
+            currency_code = selected_currency['currency_code']
+
+            # Assuming 'currency_type' is the column name in 'wallet_currency' datatable
+            currency_type_matches = app_tables.wallet_users_balance.search(currency_type=currency_code)
+
+            # Count the number of occurrences of the currency type
+            count_of_users = len(currency_type_matches)
+
+            # Update the button text with the count of users
+            self.view_user.text = f"{count_of_users} Users"
+            self.user_count_displayed = True
+        else:
+            # Reset button text to default 'view user'
+            self.view_user.text = "view number of user"
+            self.user_count_displayed = False
