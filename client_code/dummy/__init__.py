d839e75bf62b6a16b82c76f214c52ad15014453b
kohilain16@gmail.com <kohilain16@gmail.com>
2024-04-08 06:05:49 +0000

    Created dummy; Edited settings and Home

---
+++ client_code/dummy/__init__.py
@@ -0,0 +1,22 @@
+from ._anvil_designer import dummyTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+class dummy(dummyTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    open_form('Login')
+
+  def button_2_click(self, **event_args):
+    open_form('Login')
+
+  
+
