6fd0e48ab3011a4d729bd5d7f366dc19a05acc4b
kohilain16@gmail.com <kohilain16@gmail.com>
2024-04-12 06:43:27 +0000

    Edited contact_us.issue_2

--- client_code/contact_us/issue_2/__init__.py
+++ client_code/contact_us/issue_2/__init__.py
@@ -8,3 +8,10 @@ class issue_2(issue_2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    open_form('contact_us')
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
