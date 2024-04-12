8cab16896433d0277cc499fea373fabe156acf8f
kohilain16@gmail.com <kohilain16@gmail.com>
2024-04-12 07:10:59 +0000

    Edited contact_us.issue_2, contact_us.issue_3, contact_us.issue_4 and contact_us

--- client_code/contact_us/__init__.py
+++ client_code/contact_us/__init__.py
@@ -8,28 +8,25 @@ from anvil.tables import app_tables
 class contact_us(contact_usTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.card_1.visible = False
     self.card_3.visible = False
     self.card_5.visible = False
 
   def link_8_click(self, **event_args):
-    self.card_1.visible = not self.card_1.visible
-    self.card_3.visible = False
-    self.card_5.visible = False
+    open_form('Home')
 
   def link_13_click(self, **event_args):
     self.card_3.visible = not self.card_3.visible
-    self.card_1.visible = False
+    #self.card_1.visible = False
     self.card_5.visible = False
 
   def link_15_click(self, **event_args):
     self.card_5.visible = not self.card_5.visible
-    self.card_1.visible = False
+    #self.card_1.visible = False
     self.card_3.visible = False
 
   def link_16_click(self, **event_args):
     self.card_5.visible = False
-    self.card_1.visible = False
+    #self.card_1.visible = False
     self.card_3.visible = False
 
   def link_1_click(self, **event_args):
--- client_code/contact_us/issue_2/__init__.py
+++ client_code/contact_us/issue_2/__init__.py
@@ -15,3 +15,24 @@ class issue_2(issue_2Template):
   def link_2_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def button_3_click(self, **event_args):
+    open_form('Login')
+
+  def link_8_click(self, **event_args):
+    open_form('Home')
+
+  def link_16_click(self, **event_args):
+    open_form('contact_us')
