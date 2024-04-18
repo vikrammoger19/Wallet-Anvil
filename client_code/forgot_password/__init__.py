06479ede8c0d92c74e023b6c648b01e717fa5244
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-04-18 12:16:10 +0000

    Edited ServerModule1 and forgot_password

--- client_code/forgot_password/__init__.py
+++ client_code/forgot_password/__init__.py
@@ -5,16 +5,10 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import random
+from anvil import alert
 
 class forgot_password(forgot_passwordTemplate):
-    def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-        self.init_components(**properties)
-
-        # Any code you write here will run before the form opens.
-
     def button_1_click(self, **event_args):
-        """This method is called when the button is clicked"""
         # Get the email entered by the user
         user_email = self.text_box_1.text
         
@@ -22,9 +16,9 @@ class forgot_password(forgot_passwordTemplate):
         matching_users = app_tables.wallet_users.search(email=user_email)
         
         if matching_users:
-            # Email exists, generate OTP and send it
+            # Email exists, generate OTP and call the server function to send it
             otp = self.generate_otp()
-            self.send_otp_email(user_email, otp)
+            anvil.server.call('send_otp_email', user_email, otp)
             # Optionally, you can inform the user that the OTP has been sent
             alert("OTP has been sent to your email.")
         else:
@@ -36,14 +30,24 @@ class forgot_password(forgot_passwordTemplate):
         otp = ''.join(random.choice('0123456789') for _ in range(6))
         return otp
     
-    def send_otp_email(self, recipient_email, otp):
-        # Compose email message
-        subject = "Your One Time Password (OTP)"
-        message = f"Your OTP is: {otp}"
-        
-        # Send email
-        anvil.email.send(
-            to=recipient_email,
-            subject=subject,
-            text=message
-        )
+    def text_box_2_pressed_enter(self, **event_args):
+    # Get the entered OTP from text_box_2
+    entered_otp = self.text_box_2.text
+    
+    # Get the stored OTP from the server
+    stored_otp = anvil.server.call('get_stored_otp')
+    
+    print("Entered OTP:", entered_otp)
+    print("Stored OTP:", stored_otp)
+    
+    # Check if the entered OTP matches the stored OTP
+    if entered_otp == stored_otp:
+        # OTP is valid, display success message in green color
+        self.label_4.text = "OTP is valid"
+        self.label_4.foreground = "#008000"  # Green color
+    else:
+        # OTP is invalid, display error message in red color
+        self.label_4.text = "Invalid OTP"
+        self.label_4.foreground = "#FF0000"  # Red color
+
+    
