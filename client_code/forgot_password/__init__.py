38db8fa9896256e5c715020413a1b2bb7dc3fb85
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-04-19 09:39:46 +0000

    Edited forgot_password

--- client_code/forgot_password/__init__.py
+++ client_code/forgot_password/__init__.py
@@ -10,13 +10,13 @@ import time
 
 class forgot_password(forgot_passwordTemplate):
     def button_1_click(self, **event_args):
+        
         user_email = self.text_box_1.text
         
         # Check if the email exists in the database
-        matching = app_tables.wallet_users.search(email=user_email)
-        if matching:
-            self.label_3.text = "Email is valid"
-            
+        matching_users = app_tables.wallet_users.search(email=user_email)
+        
+        if matching_users:
             # Email exists, generate OTP
             otp = anvil.server.call('generate_otp')
             
@@ -25,9 +25,12 @@ class forgot_password(forgot_passwordTemplate):
             
             # Inform the user that the OTP has been sent
             alert("OTP has been sent to your email.")
+            
+            # Hide text_box_2 and label_4, and show text_box_3
+            self.text_box_2.visible = True
+            
         else:
             # Email doesn't exist, display an error message
-            self.label_3.text = "Email is invalid"
             alert("Email not found. Please enter a valid email address.")
     
     def text_box_2_pressed_enter(self, **event_args):
@@ -42,6 +45,11 @@ class forgot_password(forgot_passwordTemplate):
             # OTP is valid, display success message in green color
             self.label_4.text = "OTP is valid"
             self.label_4.foreground = "#008000"  # Green color
+            
+            # Hide text_box_3 and show text_box_4
+            self.text_box_3.visible = True
+            self.text_box_4.visible = True
+            self.label_4
         else:
             # OTP is invalid, display a message
             self.label_4.text = "Invalid OTP. Please try again."
