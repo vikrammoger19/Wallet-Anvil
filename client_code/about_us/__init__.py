05e4fb87c83f187d3bef575b3640ceac1ebd24b6
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-04-23 07:03:31 +0000

    Edited settings, about_us and signup

--- client_code/about_us/__init__.py
+++ client_code/about_us/__init__.py
@@ -8,3 +8,7 @@ class about_us(about_usTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('login')
--- client_code/signup/__init__.py
+++ client_code/signup/__init__.py
@@ -84,3 +84,50 @@ class signup(signupTemplate):
               )
               alert ("Thank you "+ self.text_box_1.text + ", for signing up! Your account has been successfully created")
               open_form('login')
+
+  def button_1_click(self, **event_args):
+        
+        user_email = self.text_box_2.text
+        
+        # Check if the email exists in the database
+        matching_users = app_tables.wallet_users.search(email=user_email)
+        
+        if matching_users:
+            # Email exists, generate OTP
+            otp = anvil.server.call('generate_otp')
+            
+            # Call the server function to send OTP via email
+            anvil.server.call('send_otp_email', user_email, otp)
+            
+            # Inform the user that the OTP has been sent
+            alert("OTP has been sent to your email.")
+            
+            # Hide text_box_2 and label_4, and show text_box_3
+            self.text_box_9.visible = True
+            
+        else:
+            # Email doesn't exist, display an error message
+            alert("Email not found. Please enter a valid email address.")
+
+  def text_box_9_pressed_enter(self, **event_args):
+        # Wait for a short duration before each attempt
+        entered_otp = self.text_box_9.text
+        
+        # Get the stored OTP from the server
+        stored_otp = anvil.server.call('get_stored_otp')
+        self.label_3_copy_copy.visible=True
+        # Check if the entered OTP matches the stored OTP
+        if entered_otp == stored_otp:
+            # OTP is valid, display success message in green color
+            self.label_3_copy_copy.text = "OTP is valid"
+            self.label_3_copy_copy.foreground = "#008000"  # Green color
+            
+            # Hide text_box_3 and show text_box_4
+            
+            
+        else:
+            # OTP is invalid, display a message
+            self.label_3_copy_copy.text = "Invalid OTP. Please try again."
+            self.label_3_copy_copy.foreground = "#FF0000"  # Red color
+            
+    
