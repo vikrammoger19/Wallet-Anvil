ee9a01b2459a1d2ab3479d34cb2efa6dc6c8bce2
govindapatro2000@gmail.com <govindapatro2000@gmail.com>
2024-04-25 08:59:02 +0000

    Edited FAQ

--- client_code/FAQ/__init__.py
+++ client_code/FAQ/__init__.py
@@ -41,66 +41,28 @@ class FAQ(FAQTemplate):
     def link_7_click(self, **event_args):
         self.link_click(6)
 
-    def link_8_click(self, **event_args):
+    def link_12_click(self, **event_args):
         self.link_click(7)
 
-    def link_9_click(self, **event_args):
+    def link_14_click(self, **event_args):
         self.link_click(8)
 
-    def link_10_click(self, **event_args):
+    def link_18_click(self, **event_args):
         self.link_click(9)
 
-    def link_11_click(self, **event_args):
+    def link_19_click(self, **event_args):
         self.link_click(10)
 
-    def link_12_click(self, **event_args):
+    def link_20_click(self, **event_args):
         self.link_click(11)
 
-    def link_13_click(self, **event_args):
+    def link_21_click(self, **event_args):
         self.link_click(12)
 
-    def link_14_click(self, **event_args):
+    def link_22_click(self, **event_args):
         self.link_click(13)
 
-    def link_15_click(self, **event_args):
+    def link_23_click(self, **event_args):
         self.link_click(14)
 
-    def link_16_click(self, **event_args):
-        open_form('contact_us')
-
-    def button_3_click(self, **event_args):
-         open_form('Login')
-
-    def link_17_click(self, **event_args):
-        open_form('about_us')
-
-    def link_18_click(self, **event_args):
-        pass
-
-    def link_19_click(self, **event_args):
-        open_form('FAQ')
-
-    def link_20_click(self, **event_args):
-        open_form('contact_us')
-
-    def home_click(self, **event_args):
-        open_form('Home')
-
-    def about_us_click(self, **event_args):
-        open_form('about_us')
-
-    def products_click(self, **event_args):
-        """This method is called when the link is clicked"""
-        pass
-
-    def contact_click(self, **event_args):
-        open_form('contact_us')
-
-    def help_click(self, **event_args):
-        pass
-
-    def button_1_click(self, **event_args):
-        open_form('Login')
-
-    def button_2_click(self, **event_args):
-        open_form('Signup')
+    
