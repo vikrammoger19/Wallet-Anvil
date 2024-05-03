c07ba4a349eb60c270c0f2c3a39a3a3885509366
kohilain16@gmail.com <kohilain16@gmail.com>
2024-05-03 10:23:24 +0000

    Created admin.adduser; Edited admin.show_users; Deleted dummy, dummy_Signup and dummy_Login

--- client_code/admin/adduser/__init__.py
+++ client_code/admin/adduser/__init__.py
@@ -1,22 +1,14 @@
-from ._anvil_designer import dummyTemplate
+from ._anvil_designer import adduserTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
-class dummy(dummyTemplate):
+
+class adduser(adduserTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    open_form('Login')
-
-  def button_2_click(self, **event_args):
-    open_form('Login')
-
-  
-
