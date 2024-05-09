809114a2a5177241049efe90729eca2db187c01d
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-05-09 08:57:25 +0000

    Created customer.transaction_history.ItemTemplate14 and customer.transaction_history.ItemTemplate15; Edited customer.transaction_history and customer.transaction_history.ItemTemplate4

--- client_code/customer/transaction_history/__init__.py
+++ client_code/customer/transaction_history/__init__.py
@@ -27,63 +27,62 @@ class transaction_history(transaction_historyTemplate):
             # Search transactions based on the user's phone number
             items = app_tables.wallet_users_transaction.search(phone=phone_number)
 
-            # Sort the items based on the 'date' column in descending order
-            sorted_items = sorted(items, key=lambda x: x['date'], reverse=True)
+            # Group transactions by date
+            grouped_transactions = {}
+            for item in items:
+                date_str = item['date'].strftime("%Y-%m-%d")  # Extract date in YYYY-MM-DD format
+                if date_str not in grouped_transactions:
+                    grouped_transactions[date_str] = {'date': date_str, 'transactions': []}
+                grouped_transactions[date_str]['transactions'].append({'fund': item['fund'], 'receiver_phone': item['receiver_phone']})
 
-            # Assuming these columns exist in 'wallet_users_transaction'
-            transaction_history_list = [
-                {
-                    'date': item['date'],
-                    'fund': item['fund'],
-                    'transaction_type': item['transaction_type'],
-                    'transaction_status': item['transaction_status'],
-                    'receiver_phone': item['receiver_phone'],
-                    'phone': item['phone']
-                }
-                for item in sorted_items
-            ]
-            
-            self.repeating_panel_1.items = transaction_history_list
+            # Create a list of dictionaries for repeating_panel_1
+            repeating_panel_1_items = []
+            for date_info in grouped_transactions.values():
+                repeating_panel_1_items.append({'date': date_info['date']})
+                for transaction in date_info['transactions']:
+                    repeating_panel_1_items.append({'fund': transaction['fund'], 'receiver_phone': transaction['receiver_phone']})
+
+            self.repeating_panel_1.items = repeating_panel_1_items
 
     def link_2_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("customer.deposit",user=self.user)
-  
+        """This method is called when the link is clicked"""
+        open_form("customer.deposit",user=self.user)
+    
     def link_3_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("customer.transfer",user=self.user)
-  
+        """This method is called when the link is clicked"""
+        open_form("customer.transfer",user=self.user)
+    
     def link_4_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("customer.withdraw",user=self.user)
-  
+        """This method is called when the link is clicked"""
+        open_form("customer.withdraw",user=self.user)
+    
     def link_7_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("customer.service",user=self.user)
-  
+        """This method is called when the link is clicked"""
+        open_form("customer.service",user=self.user)
+    
     def link_13_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("Home")
-  
+        """This method is called when the link is clicked"""
+        open_form("Home")
+    
     def link_8_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form("customer.service",user=self.user)
-  
+        """This method is called when the link is clicked"""
+        open_form("customer.service",user=self.user)
+    
     def button_3_click(self, **event_args):
-      open_form('customer', user=self.user)
+        open_form('customer', user=self.user)
 
     def link_1_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form('customer',user=self.user)
+        """This method is called when the link is clicked"""
+        open_form('customer',user=self.user)
 
     def link_24_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form('about_us')
+        """This method is called when the link is clicked"""
+        open_form('about_us')
 
     def link_25_click(self, **event_args):
-      """This method is called when the link is clicked"""
-      open_form('product')
+        """This method is called when the link is clicked"""
+        open_form('product')
 
     def primary_color_8_click(self, **event_args):
-      """This method is called when the button is clicked"""
-      pass
+        """This method is called when the button is clicked"""
+        pass
