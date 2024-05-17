0596863a63dc626725b615da8dcf2d26359fd649
govindapatro2000@gmail.com <govindapatro2000@gmail.com>
2024-05-17 11:17:04 +0000

    Edited customer_page.settings

--- client_code/customer_page/settings/__init__.py
+++ client_code/customer_page/settings/__init__.py
@@ -12,3 +12,7 @@ class settings(settingsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('customer.default_currency')
