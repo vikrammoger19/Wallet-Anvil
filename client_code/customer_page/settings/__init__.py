05b9cdfb7aa38230fcb45c837a27102f8b19d267
govindapatro2000@gmail.com <govindapatro2000@gmail.com>
2024-05-17 10:31:48 +0000

    Created customer_page.settings; Deleted customer_page.Form1

--- client_code/customer_page/settings/__init__.py
+++ client_code/customer_page/settings/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import settingsTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class settings(settingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
