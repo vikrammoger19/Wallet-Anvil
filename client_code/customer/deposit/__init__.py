25802d997f75a92823b1ff17b90053f4325c405f
kohilain16@gmail.com <kohilain16@gmail.com>
2024-05-30 09:51:47 +0000

    Edited customer.auto_topup and customer.deposit

--- client_code/customer/deposit/__init__.py
+++ client_code/customer/deposit/__init__.py
@@ -14,7 +14,7 @@ class deposit(depositTemplate):
       # Set Form properties and Data Bindings.
       username = anvil.server.call('get_username', self.user['users_phone'])
       self.label_1.text = f"Welcome to Green Gate Financial, {username}"
-      bank_names = anvil.server.call('get_user_bank_name', self.user['users_phone'])
+      bank_names = anvil.server.call('get_user_bank_name', self.user['users_account_phone'])
       self.drop_down_1.items = [str(row['bank_name']) for row in bank_names]
       self.drop_down_2.items= anvil.server.call('get_currency_code')
       self.display()
