e958c7c19e30881e281abb0901cf4f9955d30182
kohilain16@gmail.com <kohilain16@gmail.com>
2024-06-17 08:25:16 +0000

    Edited customer.wallet, admin.admin_add_user and admin.create_admin

--- client_code/admin/create_admin/__init__.py
+++ client_code/admin/create_admin/__init__.py
@@ -78,3 +78,31 @@ class create_admin(create_adminTemplate):
 
   def link_8_copy_click(self, **event_args):
     open_form('admin', user=self.user)
+
+  def link_8_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin',user=self.user)
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.report_analysis',user=self.user)
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.account_management',user=self.user)
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.transaction_monitoring',user=self.user)
+
+  def link_10_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.add_currency',user=self.user)
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.audit_trail',user=self.user)
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('customer.report_bug',user=self.user)
