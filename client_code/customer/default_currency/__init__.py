8a77cbb57666b33561143fd2831d7e8fda17f45c
vikrammoger19@gmail.com <vikrammoger19@gmail.com>
2024-06-24 03:33:15 +0000

    Edited customer.ItemTemplate17 and customer.default_currency

--- client_code/customer/default_currency/__init__.py
+++ client_code/customer/default_currency/__init__.py
@@ -130,6 +130,7 @@ class default_currency(default_currencyTemplate):
         # phone = app_tables.wallet_users.get()["phone"]
         
         self.set_default_currency(self.label_1.text,1)
+        # open_form("customer.ItemTemplate17")
 
     def link_2_click(self, **event_args):
         currency=self.label_3.text
