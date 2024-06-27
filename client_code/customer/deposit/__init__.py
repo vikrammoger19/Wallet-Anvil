42a87d7ed40cd788646237ed21d6be2bf0655af7
vikrammoger19@gmail.com <vikrammoger19@gmail.com>
2024-06-27 09:21:03 +0000

    Edited customer.Viewprofile, customer.set_limit and customer.deposit

--- client_code/customer/deposit/__init__.py
+++ client_code/customer/deposit/__init__.py
@@ -193,3 +193,7 @@ class deposit(depositTemplate):
     def link_6_click(self, **event_args):
       """This method is called when the link is clicked"""
       open_form('customer.auto_topup',user=self.user)
+
+    def text_box_2_pressed_enter(self, **event_args):
+      """This method is called when the user presses Enter in this text box"""
+      pass
