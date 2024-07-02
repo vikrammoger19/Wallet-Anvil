9a86dd014f8e23eed891efef4aa303536038d78f
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-07-02 04:25:30 +0000

    Edited customer.withdraw

--- client_code/customer/withdraw/__init__.py
+++ client_code/customer/withdraw/__init__.py
@@ -103,7 +103,6 @@ class withdraw(withdrawTemplate):
     if acc is None or cur is None:
         alert('Please enter bank details')
     else:
-        
         if self.user:
             # Retrieve user data
             user_data = app_tables.wallet_users.get(users_phone=self.user['users_phone'])
@@ -127,42 +126,33 @@ class withdraw(withdrawTemplate):
             # Check if a balance row already exists for the user
             existing_balance = app_tables.wallet_users_balance.get(users_balance_phone=self.user['users_phone'], users_balance_currency_type=cur)
 
-            if existing_balance['users_balance'] >= money_value:
+            if existing_balance and existing_balance['users_balance'] >= money_value:
                 # Update the existing balance
                 existing_balance['users_balance'] -= money_value
-            
-                # # Add a new row for the user if no existing balance
-                app_tables.wallet_users_balance.add_row(
-                    users_balance_currency_type=cur,
-                    users_balance=money_value,
-                    users_balance_phone=self.user['users_phone']
-                )
 
-            # Add a new transaction row
+                # Add a new transaction row
                 app_tables.wallet_users_transaction.add_row(
-                  users_transaction_phone=self.user['users_phone'],
-                  users_transaction_fund=money_value,
-                  users_transaction_currency=cur,
-                  users_transaction_date=current_datetime,
-                  users_transaction_bank_name=acc,
-                  users_transaction_type="Withdrawn",
-                  users_transaction_status="Wallet-Withdraw",
-                  users_transaction_receiver_phone=self.user['users_phone']
+                    users_transaction_phone=self.user['users_phone'],
+                    users_transaction_fund=money_value,
+                    users_transaction_currency=cur,
+                    users_transaction_date=current_datetime,
+                    users_transaction_bank_name=acc,
+                    users_transaction_type="Withdrawn",
+                    users_transaction_status="Wallet-Withdraw",
+                    users_transaction_receiver_phone=self.user['users_phone']
                 )
                 alert("Money withdrawn successfully from the account")
-          
             else:
-              alert("withdraw amount is more than sufficient balance")
-          
+                alert("Withdraw amount is more than the available balance")
+
             # Update the limits
             user_data['users_daily_limit'] -= money_value
             user_data['users_user_limit'] -= money_value
 
             self.populate_balances()
         else:
-            #self.label_200.text = "Error: No matching accounts found for the user or invalid account number."
             alert("Error: No matching accounts found for the user or invalid account number.")
-  
+
   def link_2_click(self, **event_args):
       """This method is called when the link is clicked"""
       open_form("customer.walletbalance",user=self.user)
