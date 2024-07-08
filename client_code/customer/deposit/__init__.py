abc5209b2b1b5b56061951cdd00c9bb6dcc01ea9
shubrathkg@gmail.com <shubrathkg@gmail.com>
2024-07-08 05:31:25 +0000

    Edited customer.deposit

--- client_code/customer/deposit/__init__.py
+++ client_code/customer/deposit/__init__.py
@@ -100,7 +100,8 @@ class deposit(depositTemplate):
         api_key = 'lPBproNAjF3AjvE2nM1obshWlRMzvdQo'
         # Set base currency and any other parameters (replace 'USD' with your desired base currency
       
-        
+        if money <= 0:
+          alert(f"payment must be atleast 1{cur}")
         if self.drop_down_1.selected_value == None:
           alert('Please select a bank account')
         elif self.drop_down_2.selected_value == None:
