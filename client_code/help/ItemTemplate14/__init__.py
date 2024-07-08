cd4fdbc80695a513a40c18cdfff0f057172ef056
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-07-08 07:05:18 +0000

    Edited help.ItemTemplate14 and help

--- client_code/help/ItemTemplate14/__init__.py
+++ client_code/help/ItemTemplate14/__init__.py
@@ -9,10 +9,22 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class ItemTemplate14(ItemTemplate14Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    if self.item:
+        self.text_area_1.text = self.item.get('users_service_query', '')
+        self.text_area_2.text = self.item.get('users_conclusion_about_query', '')
+        
+        # Conditional formatting for users_update
+        if self.item.get('users_update', False):
+            self.label_1.text = "Solved"
+            self.label_1.foreground = "green"
+        else:
+            self.label_1.text = "Pending"
+            self.label_1.foreground = "red"
+
+  
--- client_code/help/__init__.py
+++ client_code/help/__init__.py
@@ -11,26 +11,39 @@ from anvil.tables import app_tables
 
 
 class help(helpTemplate):
-  def __init__(self,user=None,**properties):
-        self.init_components(**properties)
-        self.user = user
+  def __init__(self, user=None, **properties):
+    self.init_components(**properties)
+    self.user = user
+
+    # Fetch data from wallet_users_service table
+    self.display_service_data()
+
+  def display_service_data(self):
+    # Fetch data from wallet_users_service table
+    service_data = app_tables.wallet_users_service.search()
+    
+    # Bind the data to the repeating panel
+    self.repeating_panel_1.items = service_data
 
   def button_1_click(self, **event_args):
-      query = self.text_area_1.text
-      # Fetch user information from the "users" table
-      user_info = app_tables.wallet_users.get(users_phone=self.user['users_phone'])
+    query = self.text_area_1.text
+    # Fetch user information from the "wallet_users" table
+    user_info = app_tables.wallet_users.get(users_phone=self.user['users_phone'])
 
-      if user_info is not None:
-          # Update the "Services" table with the query and user information
-          app_tables.wallet_users_service.add_row(
-              users_service_username=user_info['users_username'],
-              users_service_phone=user_info['users_phone'],
-              users_service_query=query,
-              users_service_email = user_info['users_email']
-          )
-          alert("Your query has been submitted, and our Technical Executive will get in touch with you")
-      else:
-          alert("User information not found.")
+    if user_info is not None:
+        # Update the "wallet_users_service" table with the query and user information
+        app_tables.wallet_users_service.add_row(
+            users_service_username=user_info['users_username'],
+            users_service_phone=user_info['users_phone'],
+            users_service_query=query,
+            users_service_email=user_info['users_email']
+        )
+        alert("Your query has been submitted, and our Technical Executive will get in touch with you")
+        
+        # Refresh the repeating panel data after submission
+        self.display_service_data()
+    else:
+        alert("User information not found.")
 
   def link_2_click(self, **event_args):
     """This method is called when the link is clicked"""
